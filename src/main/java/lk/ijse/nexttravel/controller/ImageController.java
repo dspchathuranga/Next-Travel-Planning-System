b037f86fc92d51d981b264793ae3297ca5d7ebbb
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-10-16 03:06:34 +0530

    files get and post response

--- src/main/java/lk/ijse/nexttravel/controller/ImageController.java
+++ src/main/java/lk/ijse/nexttravel/controller/ImageController.java
@@ -1,6 +1,7 @@
 package lk.ijse.nexttravel.controller;
 
 import lk.ijse.nexttravel.service.ImageService;
+import lk.ijse.nexttravel.util.ResponseUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +16,15 @@ public class ImageController {
 
     //handle file save req
     @PostMapping("/upload")
-    public Mono<String> uploadImage(@RequestPart("file") FilePart filePart) {
-        return imageService.SaveImageLocally(filePart);
+    public Mono<ResponseUtil> uploadImage(@RequestPart("file") FilePart filePart) {
+        return imageService.SaveImageLocally(filePart).map(savedFile ->
+                new ResponseUtil(200,"Image saved Successfully",null));
     }
 
     //handle file get req
     @GetMapping("/image/{imageId}")
-    public Mono<String> getImageUrl(@PathVariable String imageId) {
-        return imageService.getImageUrlById(imageId);
+    public Mono<ResponseUtil> getImageUrl(@PathVariable String imageId) {
+        return imageService.getImageUrlById(imageId).map(getFile->
+                new ResponseUtil(200,imageId" fetch success",getFile));
     }
 }
