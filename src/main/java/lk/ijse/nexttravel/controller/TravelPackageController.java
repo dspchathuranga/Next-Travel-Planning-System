3b9f935aa3cfe3a38b0262a47c9d41eabc8eb024
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-10-15 19:48:51 +0530

    travelpackage controller created

--- src/main/java/lk/ijse/nexttravel/controller/TravelPackageController.java
+++ src/main/java/lk/ijse/nexttravel/controller/TravelPackageController.java
@@ -1,4 +1,42 @@
 package lk.ijse.nexttravel.controller;
 
+import lk.ijse.nexttravel.dto.TravelPackageDTO;
+import lk.ijse.nexttravel.service.TravelPackageService;
+import lk.ijse.nexttravel.util.ResponseUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/package")
 public class TravelPackageController {
+
+    private final TravelPackageService packageService;
+
+    @PostMapping("/save")
+    public Mono<ResponseUtil> savePackageDetails(@RequestBody TravelPackageDTO packageDTO) {
+        return null;
+    }
+
+    @GetMapping("{packageName}")
+    public Mono<ResponseUtil> getPackageDetails(@PathVariable String packageName) {
+        return null;
+    }
+
+    @GetMapping("/getAll")
+    public Flux<ResponseUtil> getAllPackageDetails() {
+        return null;
+    }
+
+    @PutMapping("{packageId}")
+    public Mono<ResponseUtil> updatePackageDetails(@RequestBody TravelPackageDTO packageDTO,@PathVariable int packageId) {
+        return null;
+    }
+
+    @DeleteMapping("{packageId}")
+    public Mono<ResponseUtil> deletePackage(@PathVariable int packageId) {
+        return null;
+    }
 }
