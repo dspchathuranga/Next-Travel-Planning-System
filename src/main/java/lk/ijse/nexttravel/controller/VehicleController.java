b1bf05056eb3c3c0ba941bcf839651709523c6f4
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-10-15 13:44:16 +0530

    delete vehicle by id

--- src/main/java/lk/ijse/nexttravel/controller/VehicleController.java
+++ src/main/java/lk/ijse/nexttravel/controller/VehicleController.java
@@ -15,29 +15,34 @@ public class VehicleController{
 
     private final VehicleService vehicleService;
 
+    //handle vehicle post request
     @PostMapping("/save")
     public Mono<ResponseUtil> saveVehicleData(@RequestBody VehicleDTO vehicleDTO) {
         return vehicleService.saveVehicle(vehicleDTO).map(savedVehicle ->
                 new ResponseUtil(200,"Vehicle Saved",null));
     }
 
+    //handle vehicle by Id Get request
     @GetMapping("{vehicleId}")
     public Mono<ResponseUtil> getVehicleData(@PathVariable int vehicleId) {
         return vehicleService.getVehicle(vehicleId).map(vehicle->
                 new ResponseUtil(200,vehicleId+"Vehicle Retrieved",vehicle));
     }
 
+    //handle getAll vehicle request
     @GetMapping("/getAll")
     public Flux<ResponseUtil> getAllVehiclesData() {
         return vehicleService.getAllVehicles().map(vehicles ->
                 new ResponseUtil(200,"Get All vehicles",vehicles));
     }
 
+    //handle vehicle update request
     @PutMapping("{vehicleId}")
     public Mono<ResponseUtil> updateVehicleData(@RequestBody VehicleDTO vehicleDTO,@PathVariable int vehicleId) {
         return null;
     }
 
+    //handle vehicle delete by id request
     @DeleteMapping("{vehicleId}")
     public Mono<ResponseUtil> deleteVehicleData(@PathVariable int vehicleId) {
         return vehicleService.deleteVehicle(vehicleId).map(deletedVehicle ->
