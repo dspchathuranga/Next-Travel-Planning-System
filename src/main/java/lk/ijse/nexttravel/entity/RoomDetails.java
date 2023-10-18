5266e2195c330a6e16257607c1206249ccc375dd
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-10-19 02:36:54 +0530

    add entities

--- src/main/java/lk/ijse/nexttravel/entity/RoomDetails.java
+++ src/main/java/lk/ijse/nexttravel/entity/RoomDetails.java
@@ -3,6 +3,7 @@ package lk.ijse.nexttravel.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
@@ -10,4 +11,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @Document(value = "Room_details")
 public class RoomDetails {
+    @Id
+    private int room_id;
+    private String room_name;
+    private int prmeal_plane_id;
+    private int room_type_id;
+    private boolean is_AC;
+    private int adulte_count;
+    private int child_count;
+    private boolean is_pets_allow;
+    private int can_policy_id;
+    private double price_day;
+    private double price_day_discount;
+    private double price_night;
+    private double price_night_discount;
+    private String status;
 }
--- src/main/java/lk/ijse/nexttravel/entity/TravelPackagePromotion.java
+++ src/main/java/lk/ijse/nexttravel/entity/TravelPackagePromotion.java
@@ -3,11 +3,24 @@ package lk.ijse.nexttravel.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Document(value = "Travel_Package_Promotion")
 public class TravelPackagePromotion {
+    @Id
+    private int promotion_id;
+    private int travel_package_id;
+    private double hotel_discount;
+    private double guide_discount;
+    private double vehical_discount;
+    private double travel_package_discount;
+    private Date promotion_start_date;
+    private Date promotion_end_date;
+    private String promotion_status;
 }
